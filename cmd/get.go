53a16e38ef65b7aebf7c3656840300a35780119a
parthw <wparth777@gmail.com>
2021-07-26 01:57:15 +0530

    testing githooks

--- cmd/get.go
+++ cmd/get.go
@@ -33,4 +33,6 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Githooks testing comment
 }
